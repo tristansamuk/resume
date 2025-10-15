05e451de29f3d4f2ea4782e679755d3f935da7c1
Tristan Samuk <tsamuk@gmail.com>
2025-10-15 11:58:41 -0400

    Add experience seciont and position component

--- components/index.js
+++ components/index.js
@@ -1,7 +1,9 @@
+import { Experience } from './experience/experience.js';
 import { Header } from './header/header.js';
 import { Summary } from './summary/summary.js';
 
 const header = new Header('.header');
 const summary = new Summary('.summary');
+const experience = new Experience('.experience');
 
-export const components = [header, summary];
+export const components = [header, summary, experience];
