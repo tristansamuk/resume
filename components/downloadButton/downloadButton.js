40d8f52493609e42b2a206b66100bf30987b2200
Tristan Samuk <tsamuk@gmail.com>
2025-10-17 19:07:51 -0400

    Wrap async html2pdf function in try/catch block to prevent button from appearing in pdf

--- components/downloadButton/downloadButton.js
+++ components/downloadButton/downloadButton.js
@@ -1,9 +1,10 @@
-import { createTemplate, savePDF } from '../../scripts/helpers';
+import { savePDF } from '../../scripts/helpers';
 
 export class DownloadButton {
   constructor(parentClass) {
     this.parentEl = document.querySelector(parentClass);
     this.button = document.createElement('button');
+    this.button.classList.add('download-button');
     this.button.innerText = 'Download';
     this.button.addEventListener('click', savePDF);
   }
--- scripts/helpers.js
+++ scripts/helpers.js
@@ -13,8 +13,17 @@ export const formatId = (string) => {
   return string.toLowerCase().replace(/\s/g, '-');
 };
 
-export const savePDF = () => {
+export const savePDF = async () => {
+  const button = document.querySelector('.download-button');
+  button.classList.add('hidden');
+
   const resume = document.getElementById('resume');
-  console.log('this ran');
-  html2pdf(resume);
+
+  try {
+    await html2pdf(resume);
+  } catch (error) {
+    console.error(error); // replace with ui error handling
+  }
+
+  button.classList.remove('hidden');
 };
