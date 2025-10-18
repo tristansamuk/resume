d2827709cb66495172ebd7f03326d14215b9f999
Tristan Samuk <tsamuk@gmail.com>
2025-10-18 09:31:12 -0400

    Add toggleVisibility helper and refactor savePDF to include options

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -13,17 +13,31 @@ export const formatId = (string) => {
   return string.toLowerCase().replace(/\s/g, '-');
 };
 
+export const toggleVisibility = (className) => {
+  const element = document.querySelector(`${className}`);
+
+  if (element.classList.contains('hidden')) {
+    element.classList.remove('hidden');
+    return;
+  }
+
+  element.classList.add('hidden');
+};
+
 export const savePDF = async () => {
-  const button = document.querySelector('.download-button');
-  button.classList.add('hidden');
+  toggleVisibility('.download-button');
+
+  const options = {
+    margin: 0.5,
+    filename: 'tristan-samuk-resume.pdf',
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+  };
 
   const resume = document.getElementById('resume');
 
-  try {
-    await html2pdf(resume);
-  } catch (error) {
-    console.error(error); // replace with ui error handling
-  }
+  html2pdf().set(options).from(resume).save();
 
-  button.classList.remove('hidden');
+  toggleVisibility('.download-button');
 };
